c5568f962490ec8e8a3bd11d2d9d097e2eff3189
penyelamatbumi <inihanip7@gmail.com>
2026-03-10 20:38:37 +0800

    first commit

---
+++ file2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a,float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >=60)
+        return "Lolos";
+
+    else if (r >=50)
+        return "Lolos dipertimbangkan";
+    else if (r >=40)
+        return "Lolos tetapi matrikulasi 1 tahun";
+    else 
+        return "Gagal";
+}
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout <<"masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >=60)
+    //    status = "Lolos";
+    //else 
+      //  status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+
+}
